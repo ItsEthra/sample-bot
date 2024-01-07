98d0585fcfcc0d4f4176d21c09507dfe0d276760
ItsEthra <107059409+ItsEthra@users.noreply.github.com>
2024-01-07 19:42:24 +0300

    Added DELETE to rest api

--- comics-bot/src/main.rs
+++ comics-bot/src/main.rs
@@ -4,14 +4,16 @@ use axum::{
     extract::{Path, State},
     http::StatusCode,
     response::IntoResponse,
-    routing::post,
+    routing::{delete, post},
     Router,
 };
 use fred::{
     clients::RedisClient,
     interfaces::{ClientLike, HashesInterface},
 };
+use futures_util::StreamExt;
 use tokio::{fs, net::TcpListener};
+use tokio_stream::wrappers::ReadDirStream;
 
 struct Error(anyhow::Error);
 impl IntoResponse for Error {
@@ -36,14 +38,48 @@ async fn add(
     let comic_path = format!("comics/{comic}");
     fs::create_dir_all(&comic_path).await?;
 
-    let len = db.hget::<i32, _, _>("comics", &comic).await? + 1;
+    let len = db.hget::<usize, _, _>("comics", &comic).await? + 1;
 
     // Thanks ItsEthra for adding this handy `to_bytes` function, what could I have done without ya.
-    // TODO: Should be limiting and checking if body is valid image
+    // TODO: Should be limiting body size and checking if body is valid image
     let png = axum::body::to_bytes(data, usize::MAX).await?;
     fs::write(format!("comics/{comic}/{len}.png"), png).await?;
 
     db.hset("comics", (comic, len)).await?;
+
+    Ok(())
+}
+
+async fn remove(Path((comic, num)): Path<(String, usize)>) -> RouteResult<()> {
+    let stream = ReadDirStream::new(fs::read_dir(format!("comics/{comic}")).await?);
+    let to_move = stream
+        .filter_map(|x| async { x.ok() })
+        .filter_map(|de| async move {
+            let pos = de
+                .path()
+                .file_stem()
+                .unwrap()
+                .to_string_lossy()
+                .parse::<usize>()
+                .ok()?;
+            if pos == num {
+                println!("Removing {:?}", de.path().file_name().unwrap());
+
+                fs::remove_file(de.path()).await.unwrap();
+                None
+            } else {
+                (pos > num).then(|| (de.path(), pos))
+            }
+        })
+        .collect::<Vec<_>>()
+        .await;
+
+    for (path, old) in to_move {
+        let new = format!("{}.png", old - 1);
+        println!("Moving {:?} to {new}", path.file_name().unwrap());
+
+        fs::rename(path.clone(), path.with_file_name(&new)).await?;
+    }
 
     Ok(())
 }
@@ -58,6 +94,7 @@ async fn main() -> Result<()> {
     let listener = TcpListener::bind("127.0.0.1:8080").await?;
     let app = Router::new()
         .route("/add/:comic", post(add))
+        .route("/remove/:comic/:num", delete(remove))
         .with_state(client);
 
     axum::serve(listener, app).await?;
